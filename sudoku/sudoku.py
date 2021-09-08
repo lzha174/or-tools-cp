33d450a41758dec43d6569046c527775dcb8eb8b
lzha174 <min.oddo@gmail.com>
2021-09-08 22:06:33 +1200

    sudoku

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -29,10 +29,16 @@ def main():
             subsquares[(subrow, subcol)].append(var)
             vars[(i,j)] = var
     # initialise know cells
-    values = [(0,1,6), (0,2,5), (0,4,4), (0,8,1), (1,4,6),(2,0,2), (2,2,3), (2,3,1), (2,5,5), (2,6,6), (2,8,8),
-              [3,0,5], (3,1,8), (3,2,6), (3,3,3), (3,5,2),(3,7,9), (4,0,4), (4,1,3), (4,4,5), (5,3,6),(5,5,4), (5,8,3),
-              (6,2,9), (6,3,7), (6,6,3), (6,7,5), (7,0,1), (7,2,8),(7,5,9), (7,7,6), (7,8,2), (8,1,7), (8,2,4),
-              (8,3,5), (8,6,8), (8,7,1), (8,8,9)]
+
+    values = [(0,0,5), (0,5,7), (0,8,3),
+              (1,3,9),(1,4,1),
+              (2,0,1), (2,5,3), (2,6,2), (2,8,4),
+              (3,1,8), (3,3,1), (3,7,7),
+              (4,7,6),
+              (5,0,2), (5,5,4),
+              (6,2,6), (6,3,8),
+              (7,1,5), (7,8,6),
+              (8,1,7), (8,2,3), (8,3,2), (8,7,9)]
     for v in values:
         model.Add(vars[v[0],v[1]] == v[2])
 
@@ -47,7 +53,7 @@ def main():
     for key, items in subsquares.items():
         model.AddAllDifferent(items)
     solver = cp_model.CpSolver()
-    solver.parameters.linearization_level = 0
+    #solver.parameters.linearization_level = 0
     status = solver.Solve(model)
     if status == cp_model.OPTIMAL:
         print('heyhey')
